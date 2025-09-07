7171b87ff2ea0b164f43c2c05791e5fb577aa7b4
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2025-09-07 09:06:19 +0100

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "name": "Siamese Thai Spa",
+    "email": "ratsilaart@gmail.com",
+    "phoneForTel": "+441617185159",
+    "phoneFormatted": "0161 718 5159",
+    "openingHours": "Mon-Sun: 10am-10pm",
+    "onlineBooking": "https://siameasethaispa.setmore.com/",
+    "address": {
+        "lineOne": "48 Faulkner St",
+        "lineTwo": "Ground Floor",
+        "city": "Manchester",
+        "state": "",
+        "zip": "M1 4FH",
+        "country": "United Kingdom",
+        "mapLink": "https://maps.app.goo.gl/NJoHPRmbEmuJoECcA"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/SiameaseThaiSPA/?locale=en_GB",
+        "googleReview": "https://g.page/r/CSXcBC0jVfyAEBM/review",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.siamease.co.uk"
+};
