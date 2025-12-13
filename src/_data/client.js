5c45e6d8f83a487e69263ef00ff46c025a99c0e6
“andrewbramwell <andy@voucherstore.io>
2025-12-13 16:02:44 +0700

    schema

--- src/_data/client.js
+++ src/_data/client.js
@@ -21,7 +21,58 @@ module.exports = {
         "googleReview": "https://g.page/r/Ce8-snjkpTxkEBM/review",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://huahinmoonmassage.co.uk"
+    "domain": "https://huahinmoonmassage.co.uk",
+    "schema": {
+        "business": {
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            "@id": "https://huahinmoonmassage.co.uk/#business",
+            "name": "Hua Hin Moon Thai Massage",
+            "image": "https://huahinmoonmassage.co.uk/assets/svgs/logo.svg",
+            "url": "https://huahinmoonmassage.co.uk",
+            "telephone": "+441915102067",
+            "email": "huahinmoonthaimassage@gmail.com",
+            "priceRange": "££",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "16A Borough Rd, Sunniside",
+                "addressLocality": "Sunderland",
+                "postalCode": "SR1 1EQ",
+                "addressCountry": "GB"
+            },
+            "geo": {
+                "@type": "GeoCoordinates",
+                "url": "https://maps.app.goo.gl/Z9rGongCpY2aM5yq7"
+            },
+            "openingHoursSpecification": [
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                    "opens": "11:00",
+                    "closes": "20:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": "Sunday",
+                    "opens": "11:00",
+                    "closes": "18:00"
+                }
+            ],
+            "sameAs": [
+                "https://g.page/r/Ce8-snjkpTxkEBM/review"
+            ]
+        },
+        "website": {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "@id": "https://huahinmoonmassage.co.uk/#website",
+            "url": "https://huahinmoonmassage.co.uk",
+            "name": "Hua Hin Moon Thai Massage",
+            "publisher": {
+                "@id": "https://huahinmoonmassage.co.uk/#business"
+            }
+        }
+    }
 };
 
 
